afd8c0f2dc47c67d3476d0c213a21f05d9711be2
Dekatron322 <muritalaibrahim097@gmail.com>
2024-09-03 22:11:49 +0100

    add lac note

---
+++ app/(service)/lac-notes/add/page.tsx
@@ -0,0 +1,196 @@
+"use client"
+import { useState, SetStateAction } from "react"
+import DashboardNav from "components/Navbar/DashboardNav"
+import Footer from "components/Footer/Footer"
+import { IoIosArrowDropleft, IoIosArrowDropdown } from "react-icons/io"
+import { FaCloudArrowUp } from "react-icons/fa6"
+import { useDropzone, FileRejection } from "react-dropzone"
+import { useRouter } from "next/navigation"
+import Link from "next/link"
+
+// Extend the File type to include a preview property
+interface PreviewFile extends File {
+  preview: string
+}
+
+export default function AddLACNote() {
+  // Simulating user account existence with a state
+  const [hasTransactions, setHasTransactions] = useState<boolean>(true)
+
+  // Use an array of PreviewFile for files state
+  const [files, setFiles] = useState<PreviewFile[]>([])
+
+  const onDrop = (acceptedFiles: File[]) => {
+    setFiles(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+    )
+  }
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [],
+      "video/*": [],
+      "application/pdf": [],
+      "application/msword": [],
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document": [],
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": [],
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation": [],
+    },
+  })
+
+  const router = useRouter()
+
+  const handleBackButtonClick = () => {
+    router.back()
+  }
+
+  return (
+    <>
+      <section className="h-full">
+        <div className="mx-auto flex min-h-screen ">
+          <div className="flex w-full  flex-col ">
+            <div>
+              <DashboardNav />
+            </div>
+            <div className="mt-8 flex flex-row  justify-center gap-3">
+              <button
+                onClick={handleBackButtonClick}
+                className="flex h-10 items-center gap-2 rounded-md border-[1px] border-[#0085FF] p-2 text-xs"
+              >
+                <IoIosArrowDropleft className="text-xl text-[#0085FF]" />
+                GO BACK
+              </button>
+              <div className="mb-6 flex w-1/3 flex-col items-center gap-4 rounded-md border-[1px] p-4">
+                <div className="flex w-full justify-between">
+                  <p className="w-[60%] text-2xl">LAC Notes Form</p>
+                  <p className="text-xs">25 January 2024</p>
+                </div>
+                <form className=" flex w-full flex-col ">
+                  <div className="mb-3 flex w-full flex-col items-start">
+                    <label htmlFor="serviceUserName" className="label-title">
+                      Created by
+                    </label>
+                    <div className="input-field ">
+                      <input
+                        type="text"
+                        id="serviceUserName"
+                        placeholder="Type and select your name"
+                        className="w-40 bg-transparent outline-none focus:outline-none"
+                        style={{ width: "100%" }}
+                      />
+                      <IoIosArrowDropdown size={18} />
+                    </div>
+                  </div>
+
+                  <div className="mb-3 flex w-full flex-col items-start">
+                    <label htmlFor="updatedBy" className="label-title">
+                      Name of Service User
+                    </label>
+                    <div className="input-field w-40">
+                      <input
+                        type="text"
+                        id="updatedBy"
+                        placeholder="Type and select your name"
+                        className="w-40 bg-transparent outline-none focus:outline-none"
+                        style={{ width: "100%" }}
+                      />
+                      <IoIosArrowDropdown size={18} />
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-2 gap-2">
+                    <div className="mb-3 flex w-full flex-col items-start">
+                      <label htmlFor="staffOnDuty" className="label-title">
+                        Report Date
+                      </label>
+                      <div className="input-field w-40">
+                        <input
+                          type="date"
+                          id="staffOnDuty"
+                          placeholder="Separate names by comma"
+                          className="w-40 bg-transparent outline-none focus:outline-none"
+                          style={{ width: "100%" }}
+                        />
+                      </div>
+                    </div>
+                    <div className="mb-3 flex w-full flex-col items-start">
+                      <label htmlFor="staffOnDuty" className="label-title">
+                        Creation Date
+                      </label>
+                      <div className="input-field w-40">
+                        <input
+                          type="date"
+                          id="staffOnDuty"
+                          placeholder="Separate names by comma"
+                          className="w-40 bg-transparent outline-none focus:outline-none"
+                          style={{ width: "100%" }}
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mb-3 flex w-full flex-col items-start">
+                    <label htmlFor="placement" className="label-title">
+                      Staff Involved
+                    </label>
+                    <div className="input-field w-40">
+                      <input
+                        type="text"
+                        id="placement"
+                        placeholder="Select the Placement"
+                        className="w-40 bg-transparent outline-none focus:outline-none"
+                        style={{ width: "100%" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-3 flex w-full flex-col items-start">
+                    <label htmlFor="placement" className="label-title">
+                      Care Professional Involved
+                    </label>
+                    <div className="input-field w-40">
+                      <input
+                        type="text"
+                        id="placement"
+                        placeholder="Select the Placement"
+                        className="w-40 bg-transparent outline-none focus:outline-none"
+                        style={{ width: "100%" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-6 flex w-full flex-col items-start">
+                    <label htmlFor="email" className="label-title ">
+                      Details of LAC Review
+                    </label>
+                    <div className="textarea-field ">
+                      <textarea
+                        id="email"
+                        placeholder="e.g johndoe@gmail.com"
+                        className="bg-transparent outline-none focus:outline-none"
+                        style={{ width: "100%", background: "transparent" }}
+                      ></textarea>
+                    </div>
+                  </div>
+
+                  <Link
+                    href="/dashboard/post/"
+                    type="button"
+                    className="flex h-[52px] w-full items-center justify-center rounded-lg bg-[#0052FF] p-3 text-sm text-white"
+                  >
+                    SAVE AND PUBLISH
+                  </Link>
+                </form>
+              </div>
+            </div>
+
+            <Footer />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
--- app/(service)/lac-notes/page.tsx
+++ app/(service)/lac-notes/page.tsx
@@ -19,7 +19,7 @@ interface TableRow {
   keyWorker: string
 }
 
-export default function PepPlanReport() {
+export default function LACNotes() {
   const [tableData, setTableData] = useState<TableRow[]>([
     {
       id: 1,
@@ -176,10 +176,7 @@ export default function PepPlanReport() {
               <div className="mb-6 flex w-full flex-col items-center gap-4 rounded-md border-[1px] p-4">
                 <div className="flex w-full justify-between">
                   <div className="flex items-center gap-3">
-                    <Link
-                      href="/miscellaneous-reports/add"
-                      className="flex items-center gap-2 rounded-md bg-[#0085FF] px-3 py-2"
-                    >
+                    <Link href="/lac-notes/add" className="flex items-center gap-2 rounded-md bg-[#0085FF] px-3 py-2">
                       <p className="text-white max-md:px-0">Create LAC notes</p>
                       <IoAddCircleOutline className="text-white" size={20} />
                     </Link>
