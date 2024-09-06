863ad38e6113a745a1d83e7d8f10f2d8553a6d0d
Dekatron322 <muritalaibrahim097@gmail.com>
2024-09-06 21:21:56 +0100

    local authorities

---
+++ app/(local-authorities)/layout.tsx
@@ -0,0 +1,17 @@
+import SideBar from "components/Sidebar/SideBar"
+
+export const metadata = {
+  title: "Staff Users | Health Focused",
+  description: "Staff Users",
+}
+
+export default function AuthoritiesLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col-reverse border-0 border-blue-700 lg:flex-row">
+      <div className="h-16 w-full flex-none border-0 border-red-700 lg:h-full lg:w-64 lg:overflow-y-auto">
+        <SideBar />
+      </div>
+      <div className="grow overflow-y-auto border-0 border-black ">{children}</div>
+    </div>
+  )
+}
