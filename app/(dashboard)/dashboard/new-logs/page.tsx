e003aaf2bfefb7b0deea4e0fe3befb9b5cfb39b4
Dekatron322 <muritalaibrahim097@gmail.com>
2024-09-22 20:51:57 +0100

    new logs mobile

--- app/(dashboard)/dashboard/new-logs/page.tsx
+++ app/(dashboard)/dashboard/new-logs/page.tsx
@@ -57,15 +57,15 @@ export default function NewLogs() {
             <div>
               <DashboardNav />
             </div>
-            <div className="mt-8 flex flex-row  justify-center gap-3">
+            <div className="mt-8 justify-center gap-3 max-md:px-3  md:flex md:flex-row">
               <button
                 onClick={handleBackButtonClick}
-                className="flex h-10 items-center gap-2 rounded-md border-[1px] border-[#0085FF] p-2 text-xs"
+                className="flex h-10 items-center gap-2 rounded-md border-[1px] border-[#0085FF] p-2 text-xs max-md:mb-3"
               >
                 <IoIosArrowDropleft className="text-xl text-[#0085FF]" />
                 GO BACK
               </button>
-              <div className="mb-6 flex w-1/3 flex-col items-center gap-4 rounded-md border-[1px] p-4">
+              <div className="mb-6 flex flex-col items-center gap-4 rounded-md border-[1px] p-4  md:w-1/3">
                 <div className="flex w-full justify-between">
                   <p className="text-2xl">New Daily Log</p>
                   <p className="text-xs">25 January 2024</p>
@@ -159,8 +159,10 @@ export default function NewLogs() {
                     >
                       <input {...getInputProps()} />
                       <FaCloudArrowUp className="text-3xl" />
-                      <p className="text-sm">Drag and Drop files here or Browse</p>
-                      <p className="text-xs">Supported files are JPG, PNG, MP4, PDF, DOC, XLXS, PPTX</p>
+                      <p className="text-center text-sm">Drag and Drop files here or Browse</p>
+                      <p className="px-3 text-center text-xs">
+                        Supported files are JPG, PNG, MP4, PDF, DOC, XLXS, PPTX
+                      </p>
                     </div>
                     <div className="mt-4 flex flex-wrap gap-2">
                       {files.map((file) => (
